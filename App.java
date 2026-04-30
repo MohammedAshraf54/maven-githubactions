37092fd77619c623fe131768e0df28fe7f56a7b0
Bubu <163994129+bhuvan-raj@users.noreply.github.com>
2026-04-30 14:13:30 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,7 +1,12 @@
 package com.example;
 
 public class App {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello from Maven + GitHub Actions!");
+        System.out.println("Hello");
     }
 }
